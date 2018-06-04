f961afc71eb24d09b003cf9df27784ea542bbf87
techtor87 <gfaulconbridge@gmail.com>
2018-06-04 15:10:56 -0400

    Update Variable Callback

--- mortgage_optimizer.py
+++ mortgage_optimizer.py
@@ -25,13 +25,15 @@ def mortgage_payoff_schedule(   start_date, mortgage_balance,
     
     while mortgage_balance > 0:
         if curr_date.day == 1:
-            interest_due = mortgage_balance * mortgage_interest_rate
+            interest_due = mortgage_balance * mortgage_interest_rate / 12
             mortgage_interest_paid += interest_due
             principle_paid = mortgage_payment - interest_due + extra_payment_amt
             mortgage_balance = mortgage_balance - principle_paid
         
         curr_date += datetime.timedelta(days=1)
     
+    print "Standard Mortgage Paid Off"
+    print curr_date, mortgage_balance, mortgage_interest_paid
     return curr_date, mortgage_interest_paid
 
 def mortgage_optimize(start_date,
@@ -72,11 +74,11 @@ def mortgage_optimize(start_date,
         if heloc_balance >= heloc_loan_size:
             return "HELOC Limit Reached"
 
-        heloc_interest_paid -= heloc_balance * heloc_intereste_rate
-        heloc_balance += heloc_balance * heloc_intereste_rate
+        heloc_interest_paid -= heloc_balance * heloc_intereste_rate / 365
+        heloc_balance += heloc_balance * heloc_intereste_rate / 365
 
         if curr_date.day == 1:
-            interest_due = mortgage_balance * mortgage_interest_rate
+            interest_due = mortgage_balance * mortgage_interest_rate / 12
             mortgage_interest_paid += interest_due
             principle_paid = mortgage_payment - interest_due
             mortgage_balance = mortgage_balance - principle_paid
@@ -85,7 +87,7 @@ def mortgage_optimize(start_date,
         curr_date += datetime.timedelta(days=1)
 
     print "Mortgage Paid Off"
-    print curr_date, mortgage_balance, mortgage_interest_paid, heloc_balance, heloc_interest_paid
+    print curr_date, mortgage_balance, heloc_balance, mortgage_interest_paid + heloc_interest_paid
     return curr_date, mortgage_balance, heloc_balance
 
 def add_event(date, value, freq):
@@ -94,30 +96,73 @@ def add_event(date, value, freq):
     print date, value, freq
     return True
 
-if __name__ == '__main__':
-    # global mortgage_maturity
-    # global mortgage_payment
-    # global mortgage_balance
-    # global mortgage_interest_rate
-    # global heloc_intereste_rate
-    # global heloc_credit_limit
-    # global heloc_loan_size
-    # global heloc_low_limit
-    # global monthly_cashflow
-    # global start_date
+def var_update(*args):
+    global mortgage_maturity
+    global mortgage_payment
+    global mortgage_balance
+    global mortgage_interest_rate
+    global mortgage_extra_payment
+    global heloc_intereste_rate
+    global heloc_credit_limit
+    global heloc_loan_size
+    global heloc_low_limit
+    global monthly_cashflow
+    global start_date
+  
+    # mortgage_maturity = datetime.strptime(sv_mortgage_maturity.get()strftime )
+    mortgage_payment = float(sv_mortgage_payment.get())
+    mortgage_balance = float(sv_mortgage_balance.get())
+    mortgage_interest_rate = float(sv_mortgage_interest_rate.get())
+    mortgage_extra_payment = float(sv_mortgage_extra_payment.get())
+    heloc_intereste_rate = float(sv_heloc_intereste_rate.get())
+    heloc_credit_limit = float(sv_heloc_credit_limit.get())
+    heloc_loan_size = float(sv_heloc_loan_size.get())
+    heloc_low_limit = float(sv_heloc_loan_size.get())
+    # monthly_cashflow
+    # start_date = datetime.strptime(sv_mortgage_maturity.get())
+    start_date = datetime.date( datetime.date.today().year,
+                              datetime.date.today().month,
+                              1)
 
-    # mortgage_maturity = 
-    # mortgage_payment = 1923.88 - 729.74
-    # mortgage_balance = 213535.32
-    # mortgage_interest_rate = .04125 / 12
-    # heloc_intereste_rate = .0475 / 365
-    # heloc_credit_limit = -50000
-    # heloc_loan_size = 10000 
-    # heloc_low_limit = -2000
-    # monthly_cashflow = []
-    # start_date = datetime.date( datetime.date.today().year,
-    #                           datetime.date.today().month,
-    #                           1)
+    for i in monthly_cashflow:
+        i[0] = start_date + datetime.timedelta(days=i[0]-1)
+        
+    mortgage_payoff_schedule(   start_date, mortgage_balance, 
+                                mortgage_interest_rate, mortgage_payment,mortgage_extra_payment)
+        
+    mortgage_optimize(  start_date, 
+                        mortgage_balance, mortgage_interest_rate, 
+                        mortgage_payment, mortgage_maturity, 
+                        heloc_credit_limit, heloc_intereste_rate, 
+                        heloc_loan_size, heloc_low_limit, 
+                        monthly_cashflow)
+
+    return
+
+if __name__ == '__main__':
+    global mortgage_maturity
+    global mortgage_payment
+    global mortgage_balance
+    global mortgage_interest_rate
+    global heloc_intereste_rate
+    global heloc_credit_limit
+    global heloc_loan_size
+    global heloc_low_limit
+    global monthly_cashflow
+    global start_date
+
+    mortgage_maturity = datetime.date(2044,10,1)
+    mortgage_payment = 1923.88 - 729.74
+    mortgage_balance = 213535.32
+    mortgage_interest_rate = .04125 / 12
+    heloc_intereste_rate = .0475 / 365
+    heloc_credit_limit = -50000
+    heloc_loan_size = 10000 
+    heloc_low_limit = -2000
+    monthly_cashflow = []
+    start_date = datetime.date( datetime.date.today().year,
+                              datetime.date.today().month,
+                              1)
 
     monthly_cashflow = [[15,2183.08,14],
                         [8,1993.15,14],
@@ -128,66 +173,77 @@ if __name__ == '__main__':
     input_screen = TK.Tk()
     input_screen.title("Mortgage Optimizer Inputs")
     
-    mortgage_maturity = StringVar()
-    mortgage_payment = StringVar()
-    mortgage_balance = StringVar()
-    mortgage_interest_rate = StringVar()
-    mortgage_extra_payment = StringVar()
-    heloc_intereste_rate = StringVar()
-    heloc_credit_limit = StringVar()
-    heloc_loan_size = StringVar()
-    heloc_low_limit = StringVar()
-    monthly_cashflow = StringVar()
-    start_date = StringVar()
+    sv_mortgage_maturity = StringVar()
+    sv_mortgage_payment = StringVar()
+    sv_mortgage_balance = StringVar()
+    sv_mortgage_interest_rate = StringVar()
+    sv_mortgage_extra_payment = StringVar()
+    sv_heloc_intereste_rate = StringVar()
+    sv_heloc_credit_limit = StringVar()
+    sv_heloc_loan_size = StringVar()
+    sv_heloc_low_limit = StringVar()
+    # monthly_cashflow = StringVar()
+    sv_start_date = StringVar()
 
-    mortgage_maturity.set(datetime.date(2044,10,1))
-    mortgage_payment.set(1923.88 - 729.74)
-    mortgage_balance.set(213535.32)
-    mortgage_interest_rate.set(.04125)
-    mortgage_extra_payment.set( 329.18)
-    heloc_intereste_rate.set(.0475)
-    heloc_credit_limit.set(-50000)
-    heloc_loan_size.set(10000)
-    heloc_low_limit.set(-2000)
+
+    sv_mortgage_maturity.set(mortgage_maturity)
+    sv_mortgage_payment.set(mortgage_payment)
+    sv_mortgage_balance.set(mortgage_balance)
+    sv_mortgage_interest_rate.set(mortgage_interest_rate)
+    sv_mortgage_extra_payment.set( 329.18)
+    sv_heloc_intereste_rate.set(heloc_intereste_rate)
+    sv_heloc_credit_limit.set(heloc_credit_limit)
+    sv_heloc_loan_size.set(heloc_loan_size)
+    sv_heloc_low_limit.set(heloc_low_limit)
     # monthly_cashflow.set()
-    start_date.set(datetime.date( datetime.date.today().year,
-                              datetime.date.today().month,
-                              1))
+    sv_start_date.set(start_date)
+    
+    sv_mortgage_maturity.trace("w",var_update)
+    sv_mortgage_payment.trace("w",var_update)
+    sv_mortgage_balance.trace("w",var_update)
+    sv_mortgage_interest_rate.trace("w",var_update)
+    sv_mortgage_extra_payment.trace("w",var_update)
+    sv_heloc_intereste_rate.trace("w",var_update)
+    sv_heloc_credit_limit.trace("w",var_update)
+    sv_heloc_loan_size.trace("w",var_update)
+    sv_heloc_low_limit.trace("w",var_update)
+    # monthly_cashflow.trace("w",var_update)
+    sv_start_date.trace("w",var_update)
 
     # mortgage_frame = TK.Frame(input_screen).grid(row=0)
     TK.Label(input_screen,text="Mortgage Balance").grid(row=0,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=mortgage_balance).grid(row=0,column=1)
+    TK.Entry(input_screen, textvariable=sv_mortgage_balance).grid(row=0,column=1)
 
     TK.Label(input_screen,text="Mortgage Payment").grid(row=1,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=mortgage_payment).grid(row=1,column=1)
+    TK.Entry(input_screen, textvariable=sv_mortgage_payment).grid(row=1,column=1)
 
     TK.Label(input_screen,text="Mortgage Maturity").grid(row=2,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=mortgage_maturity).grid(row=2,column=1)
+    TK.Entry(input_screen, textvariable=sv_mortgage_maturity).grid(row=2,column=1)
 
     TK.Label(input_screen,text="Mortgage Interest Rate").grid(row=3,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=mortgage_interest_rate).grid(row=3,column=1) 
+    TK.Entry(input_screen, textvariable=sv_mortgage_interest_rate).grid(row=3,column=1) 
     
     TK.Label(input_screen,text="Mortgage Extra Payment").grid(row=4,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=mortgage_extra_payment).grid(row=4,column=1) 
+    TK.Entry(input_screen, textvariable=sv_mortgage_extra_payment).grid(row=4,column=1) 
     
     # heloc_frame = TK.Frame(input_screen).pack()
     TK.Label(input_screen,text="HELOC Credit Limit").grid(row=5,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=heloc_credit_limit).grid(row=5,column=1)
+    TK.Entry(input_screen, textvariable=sv_heloc_credit_limit).grid(row=5,column=1)
 
     TK.Label(input_screen,text="HELOC Interest Rate").grid(row=6,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=heloc_intereste_rate).grid(row=6,column=1)
+    TK.Entry(input_screen, textvariable=sv_heloc_intereste_rate).grid(row=6,column=1)
 
     TK.Frame(input_screen,height=10, bd=1, relief='sunken').grid(row=7)
     
     # sys_var_frame = TK.Frame(input_screen).pack()
     TK.Label(input_screen,text="HELOC Loan Size").grid(row=8,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=heloc_loan_size).grid(row=8,column=1)
+    TK.Entry(input_screen, textvariable=sv_heloc_loan_size).grid(row=8,column=1)
 
     TK.Label(input_screen,text="HELOC Low Limit").grid(row=9,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=heloc_low_limit).grid(row=9,column=1)
+    TK.Entry(input_screen, textvariable=sv_heloc_low_limit).grid(row=9,column=1)
 
     TK.Label(input_screen,text="Start Date").grid(row=10,column=0, sticky='W')
-    TK.Entry(input_screen, textvariable=start_date).grid(row=10,column=1)
+    TK.Entry(input_screen, textvariable=sv_start_date).grid(row=10,column=1)
 
     TK.Frame(input_screen,height=10, bd=1, relief='sunken').grid(row=11)
 
@@ -205,15 +261,3 @@ if __name__ == '__main__':
     TK.Listbox(cashflow_frame,selectmode='single').pack(side='bottom')
     input_screen.mainloop()
 
-    # for i in monthly_cashflow:
-    #     i[0] = curr_date + datetime.timedelta(days=i[0]-1)
-        
-    # print mortgage_payoff_schedule(   curr_date, mortgage_balance, 
-    #                             mortgage_interest_rate, mortgage_payment,mortgage_extra_payment)
-        
-    # mortgage_optimize(  curr_date, 
-    #                     mortgage_balance, mortgage_interest_rate, 
-    #                     mortgage_payment, mortgage_maturity, 
-    #                     heloc_credit_limit, heloc_intereste_rate, 
-    #                     heloc_loan_size, heloc_low_limit, 
-    #                     monthly_cashflow)
